cd1eb911a7f7240a44cd04f12d1c6c88aa8e8b61
murilonerdx <mu-silva@outlook.com>
2022-01-04 20:09:41 -0300

    Adicionando estrutura do projeto, modelos, dto, conexões com o banco, e repository

---
+++ src/main/java/br/com/alura/forum/controller/TopicosController.java
@@ -0,0 +1,30 @@
+package br.com.alura.forum.controller;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import br.com.alura.forum.controller.dto.TopicoDto;
+import br.com.alura.forum.modelo.Topico;
+import br.com.alura.forum.repository.TopicoRepository;
+
+@RestController
+public class TopicosController {
+	
+	@Autowired
+	private TopicoRepository topicoRepository;
+	
+	@RequestMapping("/topicos")
+	public List<TopicoDto> lista(String nomeCurso) {
+		if (nomeCurso == null) {
+			List<Topico> topicos = topicoRepository.findAll();
+			return TopicoDto.converter(topicos);
+		} else {
+			List<Topico> topicos = topicoRepository.findByCursoNome(nomeCurso);
+			return TopicoDto.converter(topicos);
+		}
+	}
+
+}
